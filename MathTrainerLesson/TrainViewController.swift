1221753b5830ac1d5bd305ab2228d14e4eece59f
KaterinaFomenko <katerinafomenko77@gmail.com>
2023-11-14 12:26:54 +0100

    HW-2.3 Configure answer button Version 1

--- MathTrainerLesson/TrainViewController.swift
+++ MathTrainerLesson/TrainViewController.swift
@@ -13,12 +13,81 @@ final class TrainViewController: UIViewController {
     @IBOutlet var backButton: UIButton!
     @IBOutlet var answerOneButton: UIButton!
     @IBOutlet var answerTwoButton: UIButton!
+    @IBOutlet var questionLabel: UILabel!
     
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
-            print(type)
+             switch type {
+                       case .add:
+                           sign = "+"
+                       case .divide:
+                           sign = "÷"
+                       case .multiply:
+                           sign = "×"
+                       case.subtract:
+                           sign = "-"
+                       }
+                   }
+               }
+    private var firstNumber = 0
+    private var secondNumber = 0
+    private var sign = ""
+    private var count = 0
+ 
+    var answer: Int {
+        switch type {
+        case .add:
+            return firstNumber + secondNumber
+        case .divide:
+            return firstNumber / secondNumber
+        case .multiply:
+            return firstNumber * secondNumber
+        case .subtract:
+            return firstNumber - secondNumber
         }
     }
     
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        configureQuestion()
+        let isRightButton = Bool.random()
+        var randomAnswer: Int
+        repeat {
+            randomAnswer = Int.random(in: (answer - 10)...(answer + 10))
+        } while randomAnswer == answer
+        
+        answerOneButton.setTitle(isRightButton ? String(answer) : String(randomAnswer), for: .normal)
+        answerTwoButton.setTitle(isRightButton ? String(randomAnswer) : String(answer), for: .normal)
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func leftActions(_ sender: UIButton) {
+        check(answer: sender.titleLabel?.text ?? "", for: sender)
+    }
+    @IBAction func rightAction(_ sender: UIButton) {
+        check(answer: sender.titleLabel?.text ?? "", for: sender)
+    }
+    // MARK:- Methods
+    private func configureQuestion() {
+            firstNumber = Int.random(in: 1...99)
+            secondNumber = Int.random(in: 1...99)
+            
+        let question = "\(firstNumber) \(sign) \(secondNumber) ="
+            questionLabel.text = question
+            
+        }
+    private func check(answer: String, for button: UIButton) {
+        let isRightAnswer = Int(answer) == self.answer
+        button.backgroundColor = isRightAnswer ? .green : .red
+        
+        if isRightAnswer {
+            count += 1
+            configureQuestion()
+        }
+        
+    }
+
 }
+
