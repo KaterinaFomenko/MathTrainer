ee81c0353b46b22e63ac5aac7d23b81776fc4f97
KaterinaFomenko <katerinafomenko77@gmail.com>
2024-01-02 11:58:24 +0100

    Add test commit

---
+++ MathTrainerLesson/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  MathTrainerLesson
+//
+//  Created by Katerina on 02/01/2024.
+//
+
+import Foundation
+// Train revert
+class TestFileRevert {
+    var name: String?
+}
+
+
